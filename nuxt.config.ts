655a4e66ed10f62a7a67cbb83eedba57b03214d6
bongsal12 <nhunpisal11@gmail.com>
2025-06-19 17:49:51 +0700

    no proxy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,26 +12,26 @@ export default defineNuxtConfig({
       apiBase: process.env.API_BASE_URL,
     },
   },
-  nitro: {
-    routeRules: {
-      "/sanctum/**": {
-        proxy: "http://localhost:8000/sanctum/**",
-        cors: true,
-        headers: {
-          "Access-Control-Allow-Origin": "http://localhost:3000",
-          "Access-Control-Allow-Credentials": "true",
-        },
-      },
-      "/api/**": {
-        proxy: "http://localhost:8000/api/**",
-        cors: true,
-        headers: {
-          "Access-Control-Allow-Origin": "http://localhost:3000",
-          "Access-Control-Allow-Credentials": "true",
-        },
-      },
-    },
-  },
+  // nitro: {
+  //   routeRules: {
+  //     "/sanctum/**": {
+  //       proxy: "http://localhost:8000/sanctum/**",
+  //       cors: true,
+  //       headers: {
+  //         "Access-Control-Allow-Origin": "http://localhost:3000",
+  //         "Access-Control-Allow-Credentials": "true",
+  //       },
+  //     },
+  //     "/api/**": {
+  //       proxy: "http://localhost:8000/api/**",
+  //       cors: true,
+  //       headers: {
+  //         "Access-Control-Allow-Origin": "http://localhost:3000",
+  //         "Access-Control-Allow-Credentials": "true",
+  //       },
+  //     },
+  //   },
+  // },
   modules: [
     (_options, nuxt) => {
       nuxt.hooks.hook("vite:extendConfig", (config) => {
